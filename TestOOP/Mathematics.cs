a54bf531d5183daf049731304c9e209b7a3829d3
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-09-03 17:44:14 +0100

    added general functions (empty) and come code in program to test

---
+++ TestOOP/Mathematics.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TestOOP
+{
+    public class Mathematics
+    {
+        public bool IsDivisibleBy7(int num)
+        {
+
+            return true;
+        }
+
+        public string LowerCaseMe(string testString)
+        {
+
+
+            return "";
+        }
+    }
+}
--- TestOOP/Program.cs
+++ TestOOP/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
-
+﻿
 namespace TestOOP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Beep Boop");
+            var maths = new Mathematics();
+
+            var seven = 7;
+
+            var resultOfSeven = maths.IsDivisibleBy7(seven);
+
+            if(resultOfSeven == true)
+            {
+                System.Console.WriteLine("is divible by 7");
+            }
+            else
+            {
+                System.Console.WriteLine("isnt");
+            }
+
+            var string1 = "LOWER CASE ME";
+
+            var stringResult = maths.LowerCaseMe(string1);
+
+            System.Console.WriteLine(stringResult);
         }
     }
 }
